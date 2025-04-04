f8f5bf5100c0daec0894324d57cc66e32041c744
Aleksandr Agishev <aleksandr.agishev.qubership@gmail.com>
2025-04-04 13:11:25 +0300

    Review session

--- helm-templates/qubership-apihub/generate-local-passwords.sh
+++ helm-templates/qubership-apihub/generate-local-passwords.sh
@@ -14,8 +14,8 @@ echo $(expr $(date +%N) + $(date +%s) \* $(date +%N%s) ) | base64 | cut -c $A-$B
 
 function generate-local-passwords ()
 { 
-export APIHUB_ADMIN_EMAIL=x_apihub_$(rnd 13 17)            # Optional # Default admin user login (example: x_apihub). If set - this admin user will be created automatically." 
-export APIHUB_ADMIN_PASSWORD=$(rnd 11 19)            # Optional # Default admin user password (example: password)" 
+export APIHUB_ADMIN_EMAIL=x_apihub_$(rnd 13 17)          # Optional # Default admin user login (example: x_apihub). If set - this admin user will be created automatically." 
+export APIHUB_ADMIN_PASSWORD=$(rnd 11 19)                # Optional # Default admin user password (example: password)" 
 export JWT_PRIVATE_KEY=$(cat jwt_private_key)            # Mandatory # Self generated private PKCS" 
 }
 
