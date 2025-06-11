055c10e0189070846f5eaddc22fefd70b7384701
Aleksandr Agishev <aleksandr.agishev.qubership@gmail.com>
2025-06-12 01:14:59 +0300

    Final polishing

--- helm-templates/local-k8s-quickstart/apihub-with-keycloack-quickstart.sh
+++ helm-templates/local-k8s-quickstart/apihub-with-keycloack-quickstart.sh
@@ -7,8 +7,8 @@ echo "Sleep 30 seconds, wait for PG init"
 sleep 30
 ./3-generate-secrets.sh
 ./4-deploy-keycloack.sh
-echo "Sleep 70 seconds, wait for Keycloack init"
-sleep 70
+echo "Sleep 150 seconds, wait for Keycloack init"
+sleep 150
 ./5-deploy-apihub-with-keycloack.sh
 echo "Patch APIHUB Deployments for keycloak Ingress DNS resolution"
 ./6-patch-apihub-hosts.sh
